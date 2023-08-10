b32fb4eaf35fa9855d9b7feba4177bf81503b071
RazelRaz <razel16dcm@gmail.com>
2023-08-10 20:59:51 +0600

    right sidebr

--- app/components/RightColumn.jsx
+++ app/components/RightColumn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RightColumn = () => {
     return (
         <div>
-            <p>COl-2 start</p>
+            <p>COl-2 start.</p>
             <p>col-2 end</p>
         </div>
     );
