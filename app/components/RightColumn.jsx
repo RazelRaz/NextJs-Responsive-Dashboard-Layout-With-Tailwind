f36307b0dda94c39a9db28bd7a58af61d5fc12b6
RazelRaz <razel16dcm@gmail.com>
2023-08-10 20:50:47 +0600

    right sidebar added

---
+++ app/components/RightColumn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightColumn = () => {
+    return (
+        <div>
+            <h1>RightColumn</h1>
+        </div>
+    );
+};
+
+export default RightColumn;
---
+++ app/components/SideNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideNavbar = () => {
+    return (
+        <div className='bg-gray-200 h-screen flex-none w-20'>
+            <h1>01</h1>
+        </div>
+    );
+};
+
+export default SideNavbar;
