da0b574149d88129b06d4887ac66bcf754d61648
RazelRaz <razel16dcm@gmail.com>
2023-08-12 15:13:03 +0600

    Other changes

--- app/components/RightColumn.jsx
+++ app/components/RightColumn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RightColumn = () => {
     return (
-        <div>
+        <div className='md:mt-20 flex justify-between md:w-[35%] bg-gray-400 min-h-[45%]'>
             <p>COl-2 start.</p>
             <p>col-2 end</p>
         </div>
